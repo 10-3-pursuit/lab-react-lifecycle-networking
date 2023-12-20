40d7932f7141dee6bd28b3e4fd9ed2d04831eaa9
chris.miranda <christophermiranda@pursuit.org>
2023-12-20 14:14:36 -0800

    finished the lab

--- src/Components/Employee.jsx
+++ src/Components/Employee.jsx
@@ -1,15 +1,33 @@
+import { useState } from "react";
 import PetList from "./PetList";
 import "./Employee.css";
 
-export const Employee = () => {
+export const Employee = ({ employeeData }) => {
+  const [showPets, setShowPets] = useState(false)
+  const firstName = employeeData.firstName
+  const lastName = employeeData.lastName
+  const prefix = employeeData.prefix
+  const postfix = employeeData.postfix
+
+  let fullName = firstName + ' ' + lastName
+
+  if (prefix) {
+    fullName = prefix + ' ' + fullName
+  }
+
+  if (postfix) {
+    fullName += ', ' + postfix
+  }
+
   return (
     <article className="employee">
-      <h3>Staff Member Name</h3>
-      <h4>Staff Member Title</h4>
-      <button>Show Pets</button>
-      <PetList />
+      <h3>{fullName}</h3>
+      <h4>{employeeData.title}</h4>
+      <button onClick={() => setShowPets(!showPets)}>Show Pets</button>
+      {showPets && <PetList employeeId={employeeData.id} />}
     </article>
   );
 };
 
 export default Employee;
+

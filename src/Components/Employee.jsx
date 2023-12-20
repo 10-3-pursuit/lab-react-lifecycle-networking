ce248a0929ecac137139a56300547b458b49715e
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2023-12-20 16:50:38 -0500

    Finished lab

--- src/Components/Employee.jsx
+++ src/Components/Employee.jsx
@@ -1,14 +1,36 @@
 import PetList from "./PetList";
 import "./Employee.css";
+import { useEffect, useState } from "react";
 
-export const Employee = () => {
+
+export const Employee = ({employee}) => {
+
+  const [formatedName,setFormatedName] = useState("");
+  const [showPets,setShowPets] = useState(false);
+
+  const formatName = (employee)=>{
+    let formattedName = `${employee.firstName} ${employee.lastName}`;
+    if (employee.prefix === 'Dr.') {
+      formattedName = `Dr. ${formattedName}`;
+    } 
+    if (employee.postfix === 'CVPM') {
+      formattedName = `${formattedName}, CVPM`;
+    }
+    setFormatedName(formattedName);
+  };
+
+useEffect (()=>{
+  formatName(employee);
+}, [employee])
+
+
   return (
-    <article className="employee">
-      <h3>Staff Member Name</h3>
-      <h4>Staff Member Title</h4>
-      <button>Show Pets</button>
-      <PetList />
-    </article>
+        <article className="employee" key={employee.id}>
+          <h3>{formatedName}</h3>
+          <h4>{employee.title}</h4>
+          <button onClick={()=>setShowPets(!showPets)}>Show Pets</button>
+          {showPets && <PetList employeeId ={employee.id} />}
+        </article>
   );
 };
 

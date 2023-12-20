fa683d025a3e8500b3d7ba4717357f634444aded
AnitaOwen <anitaowen@pursuit.org>
2023-12-20 15:04:02 -0500

    each employee has the correct formatted name

--- src/Components/Employee.jsx
+++ src/Components/Employee.jsx
@@ -2,11 +2,26 @@ import PetList from "./PetList";
 import "./Employee.css";
 
 export const Employee = ({employee}) => {
+
+  function calculateName(employee){
+    if(!employee.prefix && !employee.postfix){
+      return `${employee.firstName} ${employee.lastName}`
+    } else if(!employee.postfix){
+      return `${employee.prefix} ${employee.firstName} ${employee.lastName}`
+    } else if(!employee.prefix){
+      return `${employee.firstName} ${employee.lastName}, ${employee.postfix}`
+    } else{
+      return `${employee.prefix} ${employee.firstName} ${employee.lastName}, ${employee.postfix}`
+    }
+  }
+
+  const name = calculateName(employee)
+
   return (
     <article className="employee">
       <li>
-        <h3>Staff Member Name: {employee.firstName} {employee.lastName}</h3>
-        <h4>Staff Member Title {employee.title}</h4>
+        <h3>{name}</h3>
+        <h4>{employee.title}</h4>
         <button>Show Pets</button>
         <PetList />
       </li>

c6460aae15a91202fb0b14501465ffa50347c488
enochtagoe <enochtagoe@pursuit.org>
2023-12-21 08:57:41 -0500

    submit lifecylcle networking lab

--- src/Components/EmployeeList.jsx
+++ src/Components/EmployeeList.jsx
@@ -2,6 +2,14 @@ import React from "react";
 import Employee from "./Employee";
 import "./EmployeeList.css";
 
+import { useEffect, useState } from "react";
+
+const employeeList = () => {
+
+}
+
+
+
 export const EmployeeList = () => {
   return (
     <main>

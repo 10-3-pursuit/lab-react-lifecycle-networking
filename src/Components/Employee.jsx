2a0566a7fc0a9d92d2b2b6d7379e508b41e2770e
Carlitos <carlitosdutan@pursuit.org>
2023-12-20 20:25:02 -0500

    general functionality done

--- src/Components/Employee.jsx
+++ src/Components/Employee.jsx
@@ -1,13 +1,24 @@
 import PetList from "./PetList";
 import "./Employee.css";
+import { useEffect, useState } from "react";
 
-export const Employee = () => {
+export const Employee = (props) => {
+  const [displayPets, setDisplayPets] = useState(false);
+
+  const handleSubmit = () => {
+    setDisplayPets(!displayPets);
+  };
+
   return (
     <article className="employee">
-      <h3>Staff Member Name</h3>
-      <h4>Staff Member Title</h4>
-      <button>Show Pets</button>
-      <PetList />
+      <h3>
+        {props.vet.prefix} {props.vet.firstName + " "}
+        {props.vet.postfix && props.vet.lastName + ", " + props.vet.postfix}
+      </h3>
+      <h4>{props.vet.title}</h4>
+      <button onClick={handleSubmit}>Show Pets</button>
+      {console.log(displayPets)}
+      {displayPets && <PetList vetId={props.vet.id} />}
     </article>
   );
 };
--- src/Components/PetList.jsx
+++ src/Components/PetList.jsx
@@ -1,7 +1,25 @@
-export const PetList = () => {
+import { useEffect, useState } from "react";
+
+export const PetList = (props) => {
+  const [pets, setPets] = useState([]);
+
+  useEffect(() => {
+    fetch("https://resource-veterinarian-api-4guj.onrender.com/api/pets").then(
+      (res) => res.json().then((data) => setPets(data))
+    );
+  }, []);
+
   return (
     <aside className="pets-list">
-      <p>No pets listed for this employee.</p>
+      {props.displayPets ? (
+        <p>No pets listed for this employee.</p>
+      ) : pets.some((pet) => pet.employeeId === props.vetId) ? (
+        pets
+          .filter((pet) => pet.employeeId === props.vetId)
+          .map((pet) => <p key={pet.id}>{pet.name}</p>)
+      ) : (
+        <p>No pets listed for this employee.</p>
+      )}
     </aside>
   );
 };

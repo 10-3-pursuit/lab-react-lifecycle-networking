7f1676d9bd69284b30d73d5c6e00bfff8f46fc91
Cristian Garcia <crisg@sas.upenn.edu>
2023-12-21 10:10:16 -0500

    made an API call and listed out the vets on the page

--- src/Components/EmployeeList.jsx
+++ src/Components/EmployeeList.jsx
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Employee from "./Employee";
 import "./EmployeeList.css";
 
 export const EmployeeList = () => {
-  return (
-    <main>
-      <h2>All Staff</h2>
-      <section className="employee-list">
-        <Employee />
-      </section>
-    </main>
-  );
+    const [staff, setStaff] = useState([]);
+
+    useEffect(() => {
+        fetch(
+            "https://resource-veterinarian-api-4guj.onrender.com/api/employees"
+        )
+            .then((res) => res.json())
+            .then((data) => setStaff(data));
+    }, []);
+
+    return (
+        <main>
+            <h2>All Staff</h2>
+            <section className="employee-list">
+                {staff.map((employee) => (
+                    <li key={employee.id}>
+                        <Employee emp={employee} />
+                    </li>
+                ))}
+            </section>
+        </main>
+    );
 };
 
 export default EmployeeList;

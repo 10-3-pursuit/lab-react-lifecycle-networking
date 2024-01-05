f876f1a62c810cc775d30dba4e00f421b7797c80
Jennifer <jenniferpeterson@pursuit.org>
2024-01-05 13:01:20 -0500

    React Lifecycle networking Lab Complete 👩🏿‍💻

--- src/Components/EmployeeList.jsx
+++ src/Components/EmployeeList.jsx
@@ -1,13 +1,35 @@
-import React from "react";
 import Employee from "./Employee";
 import "./EmployeeList.css";
+import { useState, useEffect } from "react";
 
 export const EmployeeList = () => {
+  // create state
+  const [employees, setEmployees] = useState([]);
+
+  // create the fetch
+  const getEmployeeList = () => {
+    fetch("https://resource-veterinarian-api-4guj.onrender.com/api/employees")
+      .then((response) => response.json())
+      .then((data) => {
+        setEmployees(data);
+      })
+      .catch((err) => console.log("error"));
+  };
+
+  // console.log(employee);
+
+  useEffect(() => {
+    // call the function and remember to include a dependency in order to prevent an infinite loop
+    // in this case we only want the page to render once so therefore the dependency will be an []
+    getEmployeeList();
+  }, []);
   return (
     <main>
       <h2>All Staff</h2>
       <section className="employee-list">
-        <Employee />
+        {employees.map((employee) => (
+          <Employee key={employee.id} employee={employee} />
+        ))}
       </section>
     </main>
   );

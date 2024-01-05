d14ca6478c521dc1bb40d2a7c8d480cf39a98b27
brenda-soto <bsoto@pursuit.org>
2024-01-05 13:02:20 -0500

    Lifestyle and networking lab complete

--- src/Components/Employee.jsx
+++ src/Components/Employee.jsx
@@ -1,13 +1,27 @@
 import PetList from "./PetList";
 import "./Employee.css";
+import { useState } from "react";
 
-export const Employee = () => {
+export const Employee = ({ employee }) => {
+  const [pets, setPets] = useState(false);
+
+  const handleClick = () => {
+    setPets(!pets);
+  };
+
+  const { prefix, firstName, lastName, postfix, title, id } = employee;
+
+  let fullName = firstName + " " + lastName;
+
+  prefix && (fullName = prefix + " " + fullName);
+  postfix && (fullName = fullName + ", " + postfix);
+
   return (
     <article className="employee">
-      <h3>Staff Member Name</h3>
-      <h4>Staff Member Title</h4>
-      <button>Show Pets</button>
-      <PetList />
+      <h3>{fullName}</h3>
+      <h4>{title}</h4>
+      <button onClick={handleClick}>Show Pets</button>
+      <div>{pets && <PetList employeeId={id} />}</div>
     </article>
   );
 };

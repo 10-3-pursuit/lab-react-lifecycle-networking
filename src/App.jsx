1f8d65d23f4ce84db53e3a35e444dc514e0a116f
juligarc91 <julissagarcia@pursuit.org>
2024-01-04 22:15:12 -0500

    created PetList.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import NavBar from "./Components/NavBar";
-import EmployeeList from "./Components/EmployeeList";
+//import EmployeeList from "./Components/EmployeeList";
 
 function App () {
   return (
     <>
       <NavBar />
-      <EmployeeList />
+      {/* <EmployeeList /> */}
     </>
   );
 };
--- src/Components/PetList.jsx
+++ src/Components/PetList.jsx
@@ -1,7 +1,13 @@
-export const PetList = () => {
+// component will be used in Employee, then Employee List, then App.jsx, then index.jsx
+export const PetList = ({ pets }) => { // data stored in pets
+  // api contains array of objects with pet name located in "name" key
   return (
     <aside className="pets-list">
-      <p>No pets listed for this employee.</p>
+      {pets.length > 0 ? (
+        <p>{pets.map(pet => pet.name).join(', ')}</p>
+      ) : (
+        <p>No pets listed for this employee.</p> // ternary if employee doesn't see any pets at hospital
+      )}
     </aside>
   );
 };

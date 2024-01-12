2d7903bfaafdff0fb1781152868ea77627dd79dd
Cristian Garcia <crisg@sas.upenn.edu>
2024-01-11 23:35:08 -0500

    added a postfix for the employees that have one, and added a list of pet names for those employees that have pets

--- src/Components/Employee.jsx
+++ src/Components/Employee.jsx
@@ -1,15 +1,22 @@
 import PetList from "./PetList";
 import "./Employee.css";
+import { useState } from "react";
 
 export const Employee = ({ emp }) => {
+    const [petsButtonClicked, setPetsButtonClicked] = useState(false);
+    const showPets = () => {
+        setPetsButtonClicked(!petsButtonClicked);
+    };
+
     return (
         <article className="employee">
             <h3>
-                {emp.prefix ? emp.prefix : null} {emp.firstName} {emp.lastName}
+                {emp.prefix && emp.prefix} {emp.firstName} {emp.lastName}{" "}
+                {emp.postfix && `, ${emp.postfix}`}
             </h3>
             <h4>{emp.title}</h4>
-            <button>Show Pets</button>
-            <PetList />
+            <button onClick={showPets}>Show Pets</button>
+            {petsButtonClicked && <PetList emp={emp} />}
         </article>
     );
 };
--- src/Components/PetList.jsx
+++ src/Components/PetList.jsx
@@ -1,9 +1,27 @@
-export const PetList = () => {
-  return (
-    <aside className="pets-list">
-      <p>No pets listed for this employee.</p>
-    </aside>
-  );
+import { useEffect, useState } from "react";
+
+export const PetList = ({ emp }) => {
+    const [pets, setPets] = useState([]);
+
+    useEffect(() => {
+        fetch(
+            `https://resource-veterinarian-api-4guj.onrender.com/api/pets?employeeId=${emp.id}`
+        )
+            .then((res) => res.json())
+            .then((data) => setPets(data))
+            .catch((error) => console.error(error));
+    }, []);
+
+    return (
+        <aside className="pets-list">
+            {pets.length
+                ? pets.reduce(
+                      (acc, curr) => (acc ? acc + ", " + curr.name : curr.name),
+                      ""
+                  )
+                : "No pets found."}
+        </aside>
+    );
 };
 
 export default PetList;

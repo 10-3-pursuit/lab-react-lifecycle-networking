9eaa996b8e57c3158a338a053888fee7d7422895
enochtagoe <enochtagoe@pursuit.org>
2024-01-21 21:28:31 -0500

    push updates and submit lab

--- src/Components/PetList.jsx
+++ src/Components/PetList.jsx
@@ -1,7 +1,15 @@
-export const PetList = () => {
+
+import { useEffect, useState } from "react";
+
+
+export const PetList = ({id}) => {
+  const [pets, setPets] = useState([])
+  useEffect(() => {
+    fetch(`https://resource-veterinarian-api-4guj.onrender.com/api/pets?employeeId=${id}`).then(response => response.json()).then(data => setPets(data))
+  }, [])
   return (
     <aside className="pets-list">
-      <p>No pets listed for this employee.</p>
+      {pets.length > 0 ? pets.map(pet => pet.name).join(", ") : <p>No pets listed for this employee.</p>}
     </aside>
   );
 };
